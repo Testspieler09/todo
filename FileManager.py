1ff4bed77f7fddcc8f9ee5d6f48e571411573804
Testspieler09 <142326461+Testspieler09@users.noreply.github.com>
2024-06-10 19:43:20 +0200

    adapt code to new data.json structure
    - groups and labels now stored in task and not outside
    - tasks have a overall and group specific index now

--- FileManager.py
+++ FileManager.py
@@ -14,8 +14,7 @@ class FileManager:
     @classmethod
     def for_new_file(cls, path_with_filename_and_extension: str) -> None:
         with open(path_with_filename_and_extension, "w") as f:
-            f.write('{"labels":{},'
-                      '"groups":{},'
+            f.write('{"order of tasks in group":{},'
                       '"tasks":{}}')
         return cls(path_with_filename_and_extension)
 
@@ -53,7 +52,7 @@ class DataManager:
 
     def delete_task(self, task_hash: str) -> None:
         if task_hash not in self.data["tasks"].keys(): return
-        self.data["tasks"].pop(task_hash)
+        self.data["tasks"].remove(task_hash)
 
     def add_step(self, task_hash: str, data: dict) -> None:
         self.data["tasks"][task_hash]["steps"].update(data)
@@ -61,25 +60,23 @@ class DataManager:
     def delete_step(self, task_hash: str, step_hash: str) -> None:
         if task_hash not in self.data["tasks"].keys(): return
         if step_hash not in self.data["tasks"][task_hash]["steps"].keys(): return
-        self.data["tasks"][task_hash]["steps"].pop(step_hash)
+        self.data["tasks"][task_hash]["steps"].remove(step_hash)
 
     def change_data_step(self, task_hash: str, data: dict) -> None:
         if task_hash not in self.data["tasks"].keys(): return
         self.data["tasks"][task_hash].update(data)
 
     def add_label(self, task_hash: str, label: str) -> None:
-        if label in self.data["labels"].keys():
-            self.data["labels"][label].append(task_hash)
-        else:
-            self.data["labels"].update({label:[task_hash]})
+        if task_hash not in self.data["tasks"].keys(): return
+        self.data["tasks"][task_hash]["labels"].append(label)
 
     def delete_task_hash_from_label(self, task_hash: str, label: str) -> None:
         if label not in self.data["labels"].keys(): return
         self.data["labels"][label].remove(task_hash)
 
     def delete_label(self, label: str) -> None:
-        if label not in self.data["labels"].keys(): return
-        self.data["labels"].pop(label)
+        for value in self.data["tasks"].values():
+            if label in value["labels"]: value["labels"].remove(label)
 
     def change_importance(self, task_hash, importance: str | None) -> None:
         if task_hash not in self.data["tasks"].keys(): return
@@ -99,21 +96,15 @@ class DataManager:
 
     # GET DATA (MAINLY FOR FILTER FUNCTION)
     def get_data_with_label(self, label: str) -> dict | None:
-        if label not in self.data["labels"].keys(): return
-        task_hashes = self.data["labels"][label]
         dictionary = {}
-        for hash in task_hashes:
-            if hash not in self.data["tasks"].keys(): continue
-            dictionary.update({hash: self.data["tasks"][hash]})
+        for key, values in self.data["tasks"].items():
+            if values["labels"] == label: dictionary.update({key: values})
         return dictionary
 
     def get_data_of_group(self, group_name: str) -> dict:
-        if group_name not in self.data["groups"].keys(): return
-        task_hashes = self.data["groups"][group_name]
         dictionary = {}
-        for hash in task_hashes:
-            if hash not in self.data["tasks"].keys(): continue
-            dictionary.update({hash: self.data["tasks"][hash]})
+        for key, values in self.data["tasks"].items():
+            if values["groups"] == group_name: dictionary.update({key: values})
         return dictionary
 
     def get_data_of_importance(self, importance_lvl: str) -> dict:
@@ -125,10 +116,27 @@ class DataManager:
     def get_all_data(self) -> dict:
         return self.data["tasks"]
 
+    # Beautify Data for display purposes
+    def display_tasks(self, data: dict) -> list:
+        """
+        A function to display the tasks in a nice way.
+        e.g. Task 1 [label][label_2]
+        Only pass the tasks dict or part of it to this function otherwise it wont work
+        The function returns a list so the ScreenManager can add color based on the tasks importance.
+        """
+        output = []
+        data = dict(sorted(data.items(), key=lambda item: item[1]['index']))
+        for idx, values in enumerate(data.values()):
+            output.append(f"{idx+1}. {values['name']} {''.join(f'[{label}]' for label in values['labels'])}")
+        return output
+
+    def display_task_details(self, data: dict, task_hash: str) -> str:
+        return str(data)
+
 if __name__ == "__main__":
     from os import getcwd
     cwd = getcwd()
     filepath = split(argv[0])[0] + "\\" + "data.backup"
     f = FileManager(f"{cwd}\\data.json")
-    f.read_file_to_dict()
-    print(f.path_to_file, "\n", f.backup_path)
+    data = DataManager(f.data)
+    print(data.display_tasks(data.data["tasks"]))
